4b1a720c7355c0f6cdde855e25489a53267f2680
MKS-students <mks.students@hackreactor.com>
2016-09-08 11:15:28 -0500

    Completed pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.minKey = 0;
+  this.maxKey = 0;
+  this.storage = {};
 };
 
+Queue.prototype.size  = function() {
+  return this.count;
+};
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.maxKey] = value;
+  this.maxKey += 1;
+  this.count += 1;
+  return this.count;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count === 0) {
+    return;
+  }
+
+  var result = this.storage[this.minKey];
+  delete this.storage[this.minKey];
+  this.count -= 1;
+  this.minKey += 1;
+  return result;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,5 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.count;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count += 1;
+  return this.count;
+};
+
+Stack.prototype.pop = function() {
+  if (this.count === 0) {
+    return;
+  }
+
+  var result = this.storage[this.count-1];
+  delete this.storage[this.count - 1];
+  this.count -= 1;
+  return result;
+};
