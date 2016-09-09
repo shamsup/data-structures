72b839d6b95c54e66b483cc29410bb64012557c7
MKS-students <mks.students@hackreactor.com>
2016-09-09 13:57:47 -0500

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage[item]) {
+    return;
+  }
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: O(1)
+ * contains: O(1)
+ * remove: O(1)
  */
